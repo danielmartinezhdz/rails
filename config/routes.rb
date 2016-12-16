1c18a255f73bf6e0daa31b399da9a03fd268bc1e
Danie Martinez Hernandez <dmhdz@localhost.localdomain>
2016-12-16 16:15:39 -0600

    se modifico la pantalla de index para agregar links imagens y videos

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  post "welcome/contacto"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
